f07debd31aa1afd62315ae0061ba1300461e96e1
ruchith_a <ruchith_a@epiclanka.net>
2025-10-16 14:18:19 +0530

    added -> feedback publish

--- src/main/java/com/smartwastemanagement/controller/FeedbackController.java
+++ src/main/java/com/smartwastemanagement/controller/FeedbackController.java
@@ -1,4 +1,28 @@
 package com.smartwastemanagement.controller;
 
+
+import com.smartwastemanagement.dto.ApiResponse;
+import com.smartwastemanagement.dto.FeedbackDto;
+import com.smartwastemanagement.entity.Feedback;
+import com.smartwastemanagement.service.FeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feedback")
+@RequiredArgsConstructor
 public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+
+    @PostMapping("/publish")
+    public ResponseEntity<ApiResponse> publishFeedback(@RequestBody FeedbackDto feedbackDto){
+        return feedbackService.publishFeedback(feedbackDto);
+
+    }
+
 }
